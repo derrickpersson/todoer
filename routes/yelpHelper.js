f68c865d2b6b7db775502d230fb13ff9f623e2d0
Sen Chen <30850069+SENC275@users.noreply.github.com>
2017-12-24 00:07:20 -0800

    Update yelpHelper.js

--- routes/yelpHelper.js
+++ routes/yelpHelper.js
@@ -1,6 +1,6 @@
 var Yelp = require('yelp-v3');
 var yelp = new Yelp({
-  access_token: "Ly8AE__VAU9P5AvlbBIh6Dw15iBeLQPftWgebNJV5nR8La-VTW6XXw2yYtKeCFuTf4vEkDsFR8RRPfa6LaIXktBs7Tx7wk69CYZ7Tv1N4weMC7Uz-YYcHTq4dkQ7WnYx"
+  access_token: "API"
 });
 
 module.exports = () => {
